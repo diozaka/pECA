d1e889513a6597a9e9547d9fcafd4917b7d6ecd8
Erik Scharwächter <scharwaechter@bit.uni-bonn.de>
2020-07-02 16:47:38 +0200

    parameters and return values in docstrings

--- peca.py
+++ peca.py
@@ -13,7 +13,18 @@ TCPParamType = Tuple[np.ndarray, np.ndarray]
 
 @njit
 def tcp(X: np.ndarray, E: np.ndarray, delta: int, taus: np.ndarray) -> np.ndarray:
-    """Compute the TCP K_{tr}^{delta,taus}(E, X)."""
+    """Compute the TCP K_{tr}^{delta,taus}(E, X).
+
+    Args:
+        X: Time series.
+        E: Event series.
+        delta: Time tolerance.
+        taus: Thresholds (strictly increasing).
+
+    Returns:
+        The trigger coincidence process.
+
+    """
     T = min(len(X), len(E))
     K_tr = np.zeros_like(taus)
     for i, tau in enumerate(taus):
@@ -26,6 +37,14 @@ def _fit_gev_blockmaxima(X: np.ndarray, blocksize: int) -> Tuple:
 
     X is first split into blocks of size blocksize and the GEV distribution
     is fitted to the maxima of these blocks.
+
+    Args:
+        X: Time series.
+        blocksize: Size of the blocks. ;)
+
+    Returns:
+        All GEV parameters as returned by ss.genextreme.fit().
+
     """
     T = len(X) - (len(X)%blocksize) # ignore remainder
     Mk = np.array([X[t:t+blocksize].max() for t in range(0, T, blocksize)])
@@ -37,6 +56,15 @@ def tcp_params_fit(X: np.ndarray, delta: int, taus: np.ndarray) -> TCPParamType:
 
     The Markov model has two sets of parameters: the marginal probabilities P(K_{tr}^{delta,tau})
     and the conditional probabilities P(K_{tr}^{delta,tau_{i}} | K_{tr}^{delta,tau_{i-1}}).
+
+    Args:
+        X: Time series.
+        delta: Time tolerance.
+        taus: Thresholds (strictly increasing).
+
+    Returns:
+        Tuple with all TCP parameters (marginal and conditional probabilities).
+
     """
     gev_params = _fit_gev_blockmaxima(X, delta+1)
     ps_marginal = np.array([1.-ss.genextreme.cdf(tau, *gev_params) for tau in taus])
@@ -49,19 +77,45 @@ def tcp_marginal_expectation(N_E: int, tcp_params: TCPParamType) -> np.ndarray:
     """Compute the marginally expected TCP for an independent event series with N_E events.
 
     The marginally expected TCP contains all pointwise expected values for each threshold.
+
+    Args:
+        N_E: Number of events in the event series.
+        tcp_params: Tuple with all TCP parameters
+
+    Returns:
+        The marginal expectations.
+
     """
     return tcp_params[0]*N_E
 
 def tcp_marginal_pval(K_tr: np.ndarray, N_E: int, tcp_params: TCPParamType) -> np.ndarray:
-    """Compute the marginal p-values for the TCP K_tr under the independence assumption."""
+    """Compute the marginal p-values for the TCP K_tr under the independence assumption.
+
+    Args:
+        K_tr: Observed trigger coincidence process.
+        N_E: Number of events in the event series.
+        tcp_params: Tuple with all TCP parameters.
+
+    Returns:
+        The marginal p-values.
+
+    """
     return ss.binom.pmf(K_tr, N_E, tcp_params[0]) + ss.binom.sf(K_tr, N_E, tcp_params[0])
 
-def tcp_nll(K_tr: np.ndarray, N_E: int, tcp_params: TCPParamType, idx_start: int = 0) -> np.ndarray:
+def tcp_nll(K_tr: np.ndarray, N_E: int, tcp_params: TCPParamType, idx_start: int = 0) -> float:
     """Compute the negative log-likelihood for the TCP K_tr under the independence assumption.
 
-    N_E denotes the total number of event occurrences in the event series E and tcp_params must
-    be fitted to the time series X beforehand. The TCP can be evaluated only at higher thresholds
-    by setting idx_start > 0.
+    The TCP can be evaluated only at higher thresholds by setting idx_start > 0.
+
+    Args:
+        K_tr: Observed trigger coincidence process.
+        N_E: Number of events in the event series.
+        tcp_params: Tuple with all TCP parameters.
+        idx_start: Index of the first threshold to evaluate. Defaults to 0.
+
+    Returns:
+        The negative log-likelihood.
+
     """
     ps_marginal, ps_conditional  = tcp_params
     return -(ss.binom.logpmf(K_tr[idx_start], N_E, ps_marginal[idx_start])
